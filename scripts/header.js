47f99735da8d665ebbcb68a1c8898ecab0238173
NQAP <oliverjheng@gmail.com>
2025-03-23 22:10:40 +0800

    Update：將Darkmode與header.js合併、使用React來實作header

--- scripts/header.js
+++ scripts/header.js
@@ -1,21 +1,57 @@
-// HTML 資料夾內第一層級的 header
+'use strict';
 
-document.writeln("<header>");
-document.writeln("      <div class=\'logo\'>");
-document.writeln("          <img src=\'\' alt=\'Logo\' id=\'Logo\'>");
-document.writeln("          <span>狗來坐坐</span>");
-document.writeln("      </div>");
-document.writeln("      <div class=\'Lead_line\'>");
-document.writeln("        <nav>");
-document.writeln("          <a href=\'homepage.html\'>首頁</a> <!-- 指向首頁 -->");
-document.writeln("          <a href=\'aboutus.html\'>關於我們</a> <!-- 指向關於我們 -->");
-document.writeln("          <a href=\'dog_species.html\'>狗狗圖鑑</a> <!-- 指向成員介紹 -->");
-document.writeln("          <a href=\'Petaurus.html\'>蜜袋鼯專區</a>");
-document.writeln("        </nav>");
-document.writeln("        <div class=\'theme-switch\'>");
-document.writeln("          <label class=\'theme-label\' for=\'theme-toggle\'>深色模式</label>");
-document.writeln("          <input type=\'checkbox\' id=\'theme-toggle\' />");
-document.writeln("          <label for=\'theme-toggle\' class=\'slider\'></label>");
-document.writeln("        </div>");
-document.writeln("      </div>");
-document.writeln("    </header>");
+const element1 = (
+    <div class="Header">
+        <div class='logo'>
+            <img src='../images/dog.png' alt='Logo' id='Logo' />
+            <span>狗來坐坐</span>
+        </div>
+        <div class='Lead_line'>
+            <nav>
+                <a href='homepage.html'>首頁</a> 
+                <a href='aboutus.html'>關於我們</a>
+                <a href='dog_species.html'>狗狗圖鑑</a> 
+                <a href='Petaurus.html'>蜜袋鼯專區</a>
+            </nav>
+            <div class='theme-switch'>
+                <label class='theme-label' for='theme-toggle'>深色模式</label>
+                <input type='checkbox' id='theme-toggle' />
+                <label for='theme-toggle' class='slider'></label>
+            </div>
+        </div>
+    </div>
+);
+
+ReactDOM.render(
+  element1,
+  document.getElementById('root')
+);
+
+// 深色模式初始化
+function loadTheme() {
+    const darkMode = localStorage.getItem("darkMode");
+    const isDarkMode = darkMode === "enabled";
+    document.getElementById("theme-toggle").checked = isDarkMode;
+  
+    if (isDarkMode) {
+      document.body.classList.add("dark-mode");
+      document.getElementById('Logo').src='../images/dark_dog.png';
+    } else {
+      document.body.classList.remove("dark-mode");
+      document.getElementById('Logo').src='../images/dog.png';
+    }
+  }
+  
+  document.getElementById("theme-toggle").addEventListener("change", function () {
+    if (this.checked) {
+      document.body.classList.add("dark-mode");
+      document.getElementById('Logo').src='../images/dark_dog.png';
+      localStorage.setItem("darkMode", "enabled");
+    } else {
+      document.body.classList.remove("dark-mode");
+      document.getElementById('Logo').src='../images/dog.png';
+      localStorage.setItem("darkMode", "disabled");
+    }
+  });
+  
+  loadTheme();
